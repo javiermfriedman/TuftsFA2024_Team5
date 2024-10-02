a7aa7441b7228c88c7dab4c4ec38666a5b9998b7
javiermfriedman <147084311+javiermfriedman@users.noreply.github.com>
2024-10-01 21:53:23 -0400

    added a javi_workspace

--- Team5_Midterm_Project/Assets/Scripts/GameHandler.cs
+++ Team5_Midterm_Project/Assets/Scripts/GameHandler.cs
@@ -15,6 +15,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //populte both arrays of environment objects
         foregroundColliders = GameObject.FindGameObjectsWithTag("frontColliders");
         backgroundColliders = GameObject.FindGameObjectsWithTag("backColliders");
         
@@ -37,25 +38,30 @@ public class GameHandler : MonoBehaviour
     }
 
     public void SwapFrontBack(){
-        Debug.Log("Trying to Swap Front / Back from front is " + isFront);
+        //Debug.Log("Trying to Swap Front / Back from front is " + isFront);
         isFront = !isFront;
         player.GetComponent<PlayerOrder>().swapOrder();
         UpdateWorldColliders();
     }
 
     public void UpdateWorldColliders() {
+        //when player is in front:
         if (isFront) {
+            //go through foregound collidable objects and set them to collide and be sharp
             for (int i = 0; i < foregroundColliders.Length; i++){
                 foregroundColliders[i].GetComponent<BoxCollider2D>().enabled = true;
                 foregroundColliders[i].GetComponent<PlatformObject>().SwapSharpBlurry(isFront);
             }
+            //go through background collidble objects nd set them to not collide and be blurry
             for (int n = 0; n < backgroundColliders.Length; n++){
                 backgroundColliders[n].GetComponent<BoxCollider2D>().enabled = false;
                 backgroundColliders[n].GetComponent<PlatformObject>().SwapSharpBlurry(isFront);
             }
         }
+        //when player is in back:
         else
         {
+            //go through FG collidable objects and set them not colliders and blurry
             for (int i = 0; i < foregroundColliders.Length; i++)
             {
                 foregroundColliders[i].GetComponent<BoxCollider2D>().enabled = false;
--- Team5_Midterm_Project/Assets/Scripts/PlatformObject.cs
+++ Team5_Midterm_Project/Assets/Scripts/PlatformObject.cs
@@ -13,6 +13,7 @@ public class PlatformObject : MonoBehaviour{
     // Start is called before the first frame update
     void Start()
     {
+        //assumes player starts in front!
         if (isFrontArt)
         {
             artSharp.SetActive(true);
