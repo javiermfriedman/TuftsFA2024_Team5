794556ed3d8c391ae9c32fe6e1b6aeebcdb27f58
ellavoorhees <ella.voorhees@tufts.edu>
2024-10-14 20:21:03 -0400

    END

--- Team5_Midterm_Project/Assets/Scripts/EndColliderScript.cs
+++ Team5_Midterm_Project/Assets/Scripts/EndColliderScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndColliderScript : MonoBehaviour
 {
@@ -25,6 +26,22 @@ public class EndColliderScript : MonoBehaviour
         
     }
 
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.gameObject.CompareTag("Player"))
+    //     {
+    //         Debug.Log("Player reached the end!");
+
+    //         // Stop the player from moving
+    //         if (playerMove != null)
+    //         {
+    //             playerMove.isAlive = false;      // Stop player movement
+    //             playerMove.isAutoRunning = false; // If you want to stop auto-running as well
+    //             anim.SetBool("Win", true);
+    //         }
+    //     }
+    // }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -35,9 +52,22 @@ public class EndColliderScript : MonoBehaviour
             if (playerMove != null)
             {
                 playerMove.isAlive = false;      // Stop player movement
-                playerMove.isAutoRunning = false; // If you want to stop auto-running as well
-                anim.SetBool("Win", true);
+                playerMove.isAutoRunning = false; // Stop auto-running
+                anim.SetBool("Win", true); // Trigger backflip animation
+                
+                // Start coroutine to wait for the animation to finish
+                StartCoroutine(WaitForBackflip());
             }
         }
+    }
+
+    // Coroutine to wait for the backflip animation to finish before loading the "YouWin" scene
+    IEnumerator WaitForBackflip()
+    {
+        // Assuming the backflip animation lasts 2 seconds
+        yield return new WaitForSeconds(2.0f);
+
+        // Load the "YouWin" scene
+        SceneManager.LoadScene("WinScene");
     }
 }
